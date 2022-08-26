4ca68bd9704786bbe13dbb8721afb2c824d1c888
Apekshap16 <70598658+Apekshap16@users.noreply.github.com>
2022-08-26 18:10:14 +0530

    Uploaded movie catalog service

---
+++ MovieInfoService/movie-catalog-service.zip_expanded/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/UserRating.java
@@ -0,0 +1,25 @@
+package io.javabrains.moviecatalogservice.models;
+
+import java.util.List;
+
+public class UserRating {
+   
+	private List<Rating> userRating;
+
+	
+	
+	public UserRating() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Rating> getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(List<Rating> userRating) {
+		this.userRating = userRating;
+	}
+	
+	
+}
